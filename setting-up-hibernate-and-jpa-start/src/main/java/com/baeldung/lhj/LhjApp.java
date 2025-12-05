5e2743eba51be07c6e5a3ad808e0318a01bc9251
Vincent ATTOLODE <v.attolode@younup.fr>
2025-12-05 11:54:49 +0100

    set up hibernate and JPA configuration

--- setting-up-hibernate-and-jpa-start/src/main/java/com/baeldung/lhj/LhjApp.java
+++ setting-up-hibernate-and-jpa-start/src/main/java/com/baeldung/lhj/LhjApp.java
@@ -1,8 +1,25 @@
 package com.baeldung.lhj;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LhjApp {
 
     public static void main(final String... args) {
+        EntityManagerFactory emf = null;
+        try {
+            Logger log = LoggerFactory.getLogger(LhjApp.class);
+            log.info("Running Learn Hibernate and JPA App");
+
+            // Persistence unit name must match name defined in persistence.xml
+            emf = Persistence.createEntityManagerFactory("LHJ");
+        } finally {
+            if (emf != null && emf.isOpen()) {
+                emf.close();
+            }
+        }
 
     }
 
--- setting-up-hibernate-and-jpa-start/src/main/java/com/baeldung/lhj/persistence/model/Worker.java
+++ setting-up-hibernate-and-jpa-start/src/main/java/com/baeldung/lhj/persistence/model/Worker.java
@@ -1,7 +1,12 @@
 package com.baeldung.lhj.persistence.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Worker {
 
+    @Id
     private Long id;
 
     private String email;
