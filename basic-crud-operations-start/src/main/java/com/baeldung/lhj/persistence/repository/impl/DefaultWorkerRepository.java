9c24df6870bc4333d6804253b60ff09d85eb402e
Vincent ATTOLODE <v.attolode@younup.fr>
2025-12-08 00:55:48 +0100

    M3L1 - basic crud operations

--- basic-crud-operations-start/src/main/java/com/baeldung/lhj/persistence/repository/impl/DefaultWorkerRepository.java
+++ basic-crud-operations-start/src/main/java/com/baeldung/lhj/persistence/repository/impl/DefaultWorkerRepository.java
@@ -2,6 +2,9 @@ package com.baeldung.lhj.persistence.repository.impl;
 
 import com.baeldung.lhj.persistence.model.Worker;
 import com.baeldung.lhj.persistence.repository.WorkerRepository;
+import com.baeldung.lhj.persistence.repository.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -18,34 +21,45 @@ public class DefaultWorkerRepository implements WorkerRepository {
 
     @Override
     public Optional<Worker> findById(Long id) {
-        return workers.stream()
-            .filter(p -> p.getId().equals(id))
-            .findFirst();
+        try (EntityManager entityManager = JpaUtil.getEntityManager()) {
+            return Optional.ofNullable(entityManager.find(Worker.class, id));
+        }
     }
 
     @Override
     public Worker save(Worker worker) {
-        Long workerId = worker.getId();
-        if (workerId == null) {
-            worker.setId(ThreadLocalRandom.current().nextLong(Long.MAX_VALUE));
-        } else {
-            findById(workerId).ifPresent(workers::remove);
+        try (EntityManager entityManager = JpaUtil.getEntityManager()) {
+            entityManager.getTransaction().begin();
+            entityManager.persist(worker);
+            entityManager.getTransaction().commit();
+            return worker;
         }
-        workers.add(worker);
-        return worker;
     }
 
     @Override
     public void update(Long id, Worker worker) {
-        deleteById(id);
-        worker.setId(id);
-        workers.add(worker);
+        try (EntityManager entityManager = JpaUtil.getEntityManager()) {
+            entityManager.getTransaction().begin();
+
+            Worker retrievedWorker = entityManager.find(Worker.class, id);
+            retrievedWorker.setFirstName(worker.getFirstName());
+            retrievedWorker.setLastName(worker.getLastName());
+
+            entityManager.getTransaction().commit();
+            // No need to call entityManager.persist()
+        }
     }
 
     @Override
     public void deleteById(Long id) {
-        Worker existingWorker = findById(id).orElseThrow(IllegalArgumentException::new);
-        workers.remove(existingWorker);
+        try (EntityManager entityManager = JpaUtil.getEntityManager()) {
+            entityManager.getTransaction().begin();
+
+            Worker retrievedWorker = entityManager.find(Worker.class, id);
+            entityManager.remove(retrievedWorker);
+
+            entityManager.getTransaction().commit();
+        }
     }
 
 }
